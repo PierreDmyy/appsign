fa77e9c8c62d5bfca1eb3ce4de0f11b4402a3c5e
PierreDmyy <dumaypierre@outlook.fr>
2022-12-12 17:03:07 +0100

    add buttonGroup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import SignaturePad from 'react-signature-canvas'
-import Button from '@mui/material/Button';
-
-
-
+import ButtonGroup from '@mui/material/Button';
 import styles from './styles.module.css'
 
 class App extends Component {
@@ -22,24 +19,27 @@ class App extends Component {
   }
   render() {
     let { trimmedDataURL } = this.state
-    return <div className={styles.container}>
-         <div className='vertical-center container'>
-          <Button className={styles.buttons} variant="outlined" onClick={this.clear}>
-            Effacer
-          </Button>
-        </div>  
-        <div>
-          <br></br>
-          <Button className={styles.buttons} variant="contained" color="success" onClick={this.trim}>
-            Valider
-          </Button>
-        </div>
+    return <div className={styles.container}> 
       <div className={styles.sigContainer}>
         <SignaturePad canvasProps={{ className: styles.sigPad }}
           ref={(ref) => { this.sigPad = ref }} />
       </div>
-      <br></br>
-      
+      <div className="d-grid gap-2">
+      <ButtonGroup
+  disableElevation
+  variant="contained"
+  aria-label="Disabled elevation buttons"
+>
+  <Button>One</Button>
+  <Button>Two</Button>
+</ButtonGroup>
+              <Button className={styles.buttons} variant="outlined" onClick={this.clear}>
+            Effacer
+          </Button>
+          <Button className={styles.buttons} variant="contained" color="success" onClick={this.trim}>
+            Valider
+          </Button>
+          </div>
       <div>
         {trimmedDataURL ?
       <>
@@ -47,7 +47,6 @@ class App extends Component {
       <img alt='sign' className={styles.sigImage} src={trimmedDataURL} />
       </>
         : null }
-
       </div>
     </div>
   }
