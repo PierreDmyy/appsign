310ca9a7db5e8215e2c8d2a32f2b277b93ec2739
Pierre <36824955+PierreDmyy@users.noreply.github.com>
2022-12-12 01:10:36 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import SignaturePad from 'react-signature-canvas'
+import Button from '@mui/material/Button';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+import styles from './styles.module.css'
+
+class App extends Component {
+  state = { trimmedDataURL: null }
+  sigPad = {}
+  clear = () => {
+    this.sigPad.clear()
+  }
+  trim = () => {
+    this.setState({
+      trimmedDataURL: this.sigPad.getTrimmedCanvas()
+        .toDataURL('image/png')
+    })
+
+  }
+  render() {
+    let { trimmedDataURL } = this.state
+    return <div className={styles.container}>
+      {trimmedDataURL
+        ? <img className={styles.sigImage}
+          src={trimmedDataURL} />
+        : null}
+      <div className={styles.sigContainer}>
+        <SignaturePad canvasProps={{ className: styles.sigPad }}
+          ref={(ref) => { this.sigPad = ref }} />
+      </div>
+
+
+      <div>
+        <div>
+          <Button className={styles.buttons} variant="outlined" onClick={this.clear}>
+            Supprimer
+          </Button>
+        </div>  
+        <div>
+          <br></br>
+          <Button className={styles.buttons} variant="contained" color="success" onClick={this.trim}>
+            Enregistrer
+          </Button>
+        </div>
+
+      </div>
+    </div>
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
